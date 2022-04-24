13332db69335702af488f1dfa52626f1757de42b
ValievBulat <gennadypetrenko228@gmail.com>
2022-04-24 22:36:21 +0500

    made a test model

--- Clicker/models.py
+++ Clicker/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=255)
+    score = models.IntegerField()
+    auto_clickers = models.IntegerField()
