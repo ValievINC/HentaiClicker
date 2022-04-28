75b087842902bf1cfa344858c236c4465f100f41
ValievBulat <gennadypetrenko228@gmail.com>
2022-04-28 22:54:25 +0500

    made a clicker with changeable pictures

--- static/clicker/hentaiJS.js
+++ static/clicker/hentaiJS.js
@@ -22,3 +22,19 @@ setInterval(function () {
     score += clicks;
     document.getElementById("score").innerHTML = score;
 }, 1000)
+
+let image = document.getElementById('girl')
+let images = ["static/clicker/0points.png", "static/clicker/250kpoints.png", "static/clicker/500kpoints.png", "static/clicker/1kkpoints.png"]
+
+image.src = images[0]
+
+image.onclick = function(e){
+    if(score < 9)
+        image.src = images[0];
+    else if (score < 19)
+        image.src = images[1];
+    else if (score < 29)
+        image.src = images[2];
+    else if (score < 39)
+        image.src = images[3];
+}
